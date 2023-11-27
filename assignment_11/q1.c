64fd8e7588b63acb50345de4e3690a2a2d1a4778
Himanshu Kumare <125734156+xpnsn@users.noreply.github.com>
2023-11-27 09:46:20 +0000

    ass11

---
+++ assignment_11/q1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+void swap(char *x, char *y){
+    char temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permute(char *a, int i, int n) {
+    int j;
+    if (i == n) {
+        printf("%s\n", a);
+    } else {
+        for (j = i; j <= n; j++) {
+            swap((a + i), (a + j));
+            permute(a, i + 1, n);
+            swap((a + i), (a + j));
+        }
+    }
+}
+
+int main() {
+    char a[20];
+    int n;
+    printf("enter the string : ");
+    scanf("%s", a);
+    n = strlen(a);
+    printf("permutation\n");
+    permute(a, 0, n - 1);
+    return 0;
+}
